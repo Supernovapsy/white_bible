5e55c773a2aa241103fe9afa8345b95b65bb8056
Supernovapsy <tonywli@hotmail.com>
2016-05-27 21:57:55 -0400

    updates

--- 1/1.1.c
+++ 1/1.1.c
@@ -2,11 +2,21 @@
 // defined in the ANSI standard.
 #include <stdio.h>
 
+// This function defaults to int as the return type.
+test()
+{
+    return 0;
+}
+
 // The "main" function is special: it is the starting point for all C programs.
-main()
+main() // Compiler makes the return type default to int.
 {
     // While C is free-form, it does not allow strings to extend over two
     // lines with a single pair of double-quotes.
     printf("hello world!\n")
     ; // Although many other parts of the program can.
+    // \c translates to just c. Since '\c' is unrecognized, the backslash is
+    // ignored.
+    printf(
+        "%d\c\n", test());
 }
